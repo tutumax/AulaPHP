3cd0e7f606a4feee9fab83e26908fabd82344a6b
Arthur Máximo <arthurmaximo62@gmail.com>
2025-10-23 12:26:44 -0300

    23/10/2025

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+    
+    <?php include "menu.php"?>
+
+    <div class="conteudo">
+        <h3>Integração do PHP com MySql</h3>
+
+        <p>Nesta aula iremos estudar como integrar/contectar o PHP com o MySQL.</p>
+        <p>O objetivo deste projeto será cadastrar alunos de uma escola</p>
+    </div>
+
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu</title>
+</head>
+<body>
+    <div class ="menu">
+        <div>
+            <a href="#">Aula BD</a>
+        </div>
+        <div >
+            <a href="#">Cadastrar</a>
+            <a href="#">Consultar</a>
+        </div>
+    </div>
+</body>
+</html>
