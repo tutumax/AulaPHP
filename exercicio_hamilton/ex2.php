60d6dfb2532b33864413cb5f0b280f32d3e6b49c
Arthur Máximo <arthurmaximo62@gmail.com>
2026-02-26 12:15:36 -0300

    Update ex2.php

--- exercicio_hamilton/ex2.php
+++ exercicio_hamilton/ex2.php
@@ -6,7 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    <form method='POST' name='sistema' action='ex1.php'>
+    <form method='POST' name='sistema' action='ex2.php'>
         <p>Digite o seu salário base?</p> <br>
         <input type='text' name='salario'><br>
 
@@ -33,15 +33,48 @@
         $numero = $_REQUEST['numero'];
         $nome = $_REQUEST['nome'];
 
-        $salario_bruto = $salario + ($horasextras * $valorextras)+($numero * 45,00)
+        $salario_bruto = $salario + ($horasextras * $valorextras)+($numero * 45.00);
 
-        if($salario_bruto < 1659,38){
+        if($salario_bruto < 1659.38){
             $inss = $salario_bruto * 0.08;
         }
-        elseif($salario_bruto ) 1659,38 && 2.826){
-            $inss = $salario_bruto * 0.08;
+        elseif($salario_bruto > 1659.38 && $salario_bruto < 2765){
+            $inss = $salario_bruto * 0.09;
         }
-    }
+        elseif($salario_bruto > 2765 && $salario_bruto < 5531){
+            $inss = $salario_bruto * 0.11;
+        }
+        elseif($salario_bruto > 5531){
+            $inss = 608.44;
+        };
+
+        if($salario_bruto <1903.98){
+            $ir = 0;
+        }
+        elseif($salario_bruto > 1903.98 && $salario_bruto < 2826){
+            $ir = $salario_bruto * 0.075;
+        }
+        elseif($salario_bruto > 2826 && $salario_bruto < 3751){
+            $ir = $salario_bruto * 0.15;
+        }
+        elseif($salario_bruto > 3751 && $salario_bruto < 4664.68){
+            $ir = $salario_bruto * 0.225;
+        }
+        elseif($salario_bruto > 4664.68){
+            $ir = $salario_bruto * 0.275;
+        };
+
+
+        
+
+
+
+        $salarioliquido = $salario_bruto - $inss - $ir;
+
+        echo"Ola $nome, seu salario base: $salario, seu salario base: $salario_bruto ; suas horas extras: $horasextras e o valor: $valorextras ; seu número de dependentes: $numero ;
+        seu inss : $inss ; seu ir : $ir ; salario final: $salarioliquido";
+
+    }   
 
 ?>
 </html>
