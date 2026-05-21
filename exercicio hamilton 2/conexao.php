8326f355468466d989ba6880155f43b0eb621c58
Arthur Máximo <arthurmaximo62@gmail.com>
2026-05-21 10:49:27 -0300

    Fix PDO connection setup in conexao.php

--- exercicio hamilton 2/conexao.php
+++ exercicio hamilton 2/conexao.php
@@ -5,8 +5,8 @@
     $dbname = "empresa";
 
     try{
-        $conm = new PDO();
-        
+        $conn = new PDO("mysql:host=$server_name;dbname=$dbname;charset=UTF8",$username , $password);  
+        $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
     }
     catch(PDOException $erro){
         header("Ocorreu o seguinte erro:" .$erro->getMessage());
